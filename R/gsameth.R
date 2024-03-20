fc158b05cecd976bc205cc40bbc97ed305070fa8
Calandra Grima <calandragrima@gmail.com>
2024-03-20 15:28:00 +1100

    Add support for EPICv2 array to gsameth.R

--- R/gsameth.R
+++ R/gsameth.R
@@ -33,7 +33,9 @@
 #' Genes associated with each CpG site are obtained from the annotation 
 #' package \code{IlluminaHumanMethylation450kanno.ilmn12.hg19} if the array 
 #' type is "450K". For the EPIC array, the annotation package
-#' \code{IlluminaHumanMethylationEPICanno.ilm10b4.hg19} is used. To use a
+#' \code{IlluminaHumanMethylationEPICanno.ilm10b4.hg19} is used. For the EPIC v2 
+#' array, the annotation package 
+#' \code{IlluminaHumanMethylationEPICv2anno.20a1.hg38} is used. To use a
 #' different annotation package, please supply it using the \code{anno}
 #' argument. 
 #' 
@@ -51,8 +53,8 @@
 #' sites on the array.
 #' @param collection A list of user specified gene sets to test. Can also be a
 #' single character vector gene set. Gene identifiers must be Entrez Gene IDs.
-#' @param array.type The Illumina methylation array used. Options are "450K" or
-#' "EPIC". Defaults to "450K".
+#' @param array.type The Illumina methylation array used. Options are "450K", 
+#' "EPIC" or "EPICv2". Defaults to "450K".
 #' @param plot.bias Logical, if true a plot showing the bias due to the
 #' differing numbers of probes per gene will be displayed
 #' @param prior.prob Logical, if true will take into account the probability of
@@ -159,7 +161,7 @@
 #' @import IlluminaHumanMethylation450kanno.ilmn12.hg19 IlluminaHumanMethylationEPICanno.ilm10b4.hg19
 #' @export gsameth
 gsameth <- function(sig.cpg, all.cpg=NULL, collection, 
-                    array.type = c("450K","EPIC"), plot.bias=FALSE, 
+                    array.type = c("450K","EPIC", "EPICv2"), plot.bias=FALSE, 
                     prior.prob=TRUE, anno=NULL, equiv.cpg = TRUE,
                     fract.counts = TRUE, 
                     genomic.features = c("ALL", "TSS200","TSS1500","Body",
@@ -176,7 +178,7 @@ gsameth <- function(sig.cpg, all.cpg=NULL, collection,
   
   if(!is.vector(sig.cpg))
     stop("Input CpG list is not a character vector")
-  array.type <- match.arg(toupper(array.type),c("450K","EPIC"))
+  array.type <- match.arg(toupper(array.type),c("450K","EPIC", "EPICv2"))
   genomic.features <- match.arg(genomic.features, c("ALL", "TSS200","TSS1500",
                                                     "Body", "1stExon","3'UTR",
                                                     "5'UTR","ExonBnd"), 

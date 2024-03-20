cd2047085516c42529da13bf5969f5b922af8492
Calandra Grima <calandragrima@gmail.com>
2024-03-20 15:28:28 +1100

    Add support for EPICv2 array to goregion.R

--- R/goregion.R
+++ R/goregion.R
@@ -24,7 +24,9 @@
 #' Genes associated with each CpG site are obtained from the annotation
 #' package \code{IlluminaHumanMethylation450kanno.ilmn12.hg19} if the array
 #' type is "450K". For the EPIC array, the annotation package
-#' \code{IlluminaHumanMethylationEPICanno.ilm10b4.hg19} is used. To use a
+#' \code{IlluminaHumanMethylationEPICanno.ilm10b4.hg19} is used. For the EPIC v2 
+#' array, the annotation package 
+#' \code{IlluminaHumanMethylationEPICv2anno.20a1.hg38} is used. To use a
 #' different annotation package, please supply it using the \code{anno}
 #' argument. 
 #' 
@@ -50,8 +52,8 @@
 #' sites on the array.
 #' @param collection The collection of pathways to test. Options are "GO" and
 #' "KEGG". Defaults to "GO".
-#' @param array.type The Illumina methylation array used. Options are "450K" or
-#' "EPIC". Defaults to "450K".
+#' @param array.type The Illumina methylation array used. Options are "450K", 
+#' "EPIC" or "EPICv2". Defaults to "450K".
 #' @param plot.bias Logical, if true a plot showing the bias due to the
 #' differing numbers of probes per gene will be displayed.
 #' @param prior.prob Logical, if true will take into account the probability of
@@ -192,7 +194,7 @@
 #' 
 #' @export goregion
 goregion <- function(regions, all.cpg=NULL, collection=c("GO","KEGG"), 
-                     array.type = c("450K","EPIC"), plot.bias=FALSE, 
+                     array.type = c("450K","EPIC", "EPICv2"), plot.bias=FALSE, 
                      prior.prob=TRUE, anno=NULL, equiv.cpg = TRUE,
                      fract.counts = TRUE, 
                      genomic.features = c("ALL", "TSS200","TSS1500","Body",
@@ -204,7 +206,7 @@ goregion <- function(regions, all.cpg=NULL, collection=c("GO","KEGG"),
   # Jovana Maksimovic
   # 26 April 2019. Last updated 18 June 2021.
 {
-    array.type <- match.arg(toupper(array.type), c("450K","EPIC"))    
+    array.type <- match.arg(toupper(array.type), c("450K","EPIC", "EPICv2"))    
     collection <- match.arg(toupper(collection), c("GO","KEGG"))
     genomic.features <- match.arg(genomic.features, c("ALL", "TSS200","TSS1500",
                                                       "Body", "1stExon","3'UTR",
@@ -225,6 +227,8 @@ goregion <- function(regions, all.cpg=NULL, collection=c("GO","KEGG"),
   if(is.null(anno)){
     if(array.type=="450K"){
       anno <- minfi::getAnnotation(IlluminaHumanMethylation450kanno.ilmn12.hg19::IlluminaHumanMethylation450kanno.ilmn12.hg19)
+    } else if (array.type=="EPICv2") {
+      anno <- minfi::getAnnotation(IlluminaHumanMethylationEPICv2anno.20a1.hg38::IlluminaHumanMethylationEPICv2anno.20a1.hg38)
     } else {
       anno <- minfi::getAnnotation(IlluminaHumanMethylationEPICanno.ilm10b4.hg19::IlluminaHumanMethylationEPICanno.ilm10b4.hg19)
     }
